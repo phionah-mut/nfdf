4a089219b390af8ce0333d62391cdc219f29db85
tuyizereyves01-cell <tuyizereyves01@gmail.com>
2026-09-19 23:03:30 +0000

    calc

---
+++ programmimgwithc/calc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1, num2, result;
+    char operator;
+    int valid = 1;
+
+    printf("Enter the first number: ");
+    if (scanf("%f", &num1) != 1)
+    {
+        printf("Error: please enter a valid number.\n");
+        return 1;
+    }
+
+  
+}
